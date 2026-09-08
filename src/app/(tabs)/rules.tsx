78b5d748ef6f169e22cae7786f70513539af0434
Constantin Meier <meierconsti@web.de>
2026-09-08 10:10:17 +0200

    Started desinging the rules tab

--- src/app/(tabs)/rules.tsx
+++ src/app/(tabs)/rules.tsx
@@ -1,11 +1,78 @@
-import {View, Text} from "react-native";
-
+import {View, Text, ScrollView, Pressable} from "react-native";
+import {Trash, Plus} from "lucide-react-native"
+import { Button } from 'react-native-paper';
 
 export default function RulesPage() {
 
     return (
-        <View>
-            <Text className="bg-clip-text bg-linear-to-br text-transparent from-green-300 to-olive-500">Rules</Text>
+        <View className="items-center justify-start flex-1 flex-col p-2 pt-10">
+            <Text className="text-4xl font-bold text-orange-500 pb-10">Rules</Text>
+            <View className="flex gap-5">
+                <View className="w-full max-h-70 flex-1 justify-start items-center">
+                    <Text className="text-lg border-b-2">Allowed countries</Text>
+                    <ScrollView className="w-full py-2" contentContainerClassName="gap-2 pb-10" showsVerticalScrollIndicator={false}>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                    </ScrollView>
+                    <View className="flex flex-row items-end justify-end w-full">
+                        <Button  mode="contained" className="flex flex-row items-center justify-center">Add countries</Button>
+                    </View>
+                </View>
+                <View className="w-full max-h-70 flex-1 justify-start items-center">
+                    <Text className="text-lg border-b-2">Blocked countries</Text>
+                    <ScrollView className="w-full py-2" contentContainerClassName="gap-2 pb-10" showsVerticalScrollIndicator={false}>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                        <View className="h-15 w-full flex-row items-center justify-between p-2 border-y-2">
+                            <Text className="text-3xl">Germany</Text>
+                            <Trash size={35} color={"#f44336"}/>
+                        </View>
+                    </ScrollView>
+                    <View className="flex flex-row items-end justify-end w-full">
+                        <Button  mode="contained" className="flex flex-row items-center justify-center">Add countries</Button>
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
--- src/components/blockRoamingButton.tsx
+++ src/components/blockRoamingButton.tsx
@@ -1,4 +1,4 @@
-import {Pressable, View, Animated, Easing} from "react-native";
+import {Pressable, View, Animated, Easing, Text} from "react-native";
 import Svg, { Path } from "react-native-svg";
 import {useEffect, useRef, useState} from "react";
 import {Shield, ShieldOff} from "lucide-react-native"
@@ -65,31 +65,33 @@ export default function BlockRoamingButton({title, onPress}: {title: string, onP
     const rotation = rotationRef.interpolate({inputRange: [0,1], outputRange: ["0deg", "360deg"]})
 
     return(
-        <Pressable onPress={setActiveAction} >
-            {({pressed }) => (
-                <View className="relative flex items-center justify-center w-60 h-60">
-                    <Animated.View className="w-60 h-60 absolute inset-0" style={{
-                        transform: [
-                            { rotate: rotation },
-                            { scale: active ? 1 : 0.90 },
-                        ],
-                    }}>
-                        <Svg viewBox="0 0 250 250" width="100%" height="100%" className="absolute inset-0">
-                        <Path
-                        d="M151.572 33.2059C192.835 15.2847 234.715 57.165 216.794 98.4282L213.831 105.251C208.36 117.848 208.36 132.152 213.831 144.749L216.794 151.572C234.715 192.835 192.835 234.715 151.572 216.794L144.749 213.831C132.152 208.36 117.848 208.36 105.251 213.831L98.4281 216.794C57.165 234.715 15.2847 192.835 33.2059 151.572L36.169 144.749C41.6404 132.152 41.6404 117.848 36.1689 105.251L33.2059 98.4281C15.2847 57.165 57.165 15.2847 98.4282 33.2059L105.251 36.169C117.848 41.6404 132.152 41.6404 144.749 36.1689L151.572 33.2059Z"
-                        fill={active ? "#fba32b" : "#775a32"}
-                        />
-                        </Svg>
-                    </Animated.View>
-                    {active ? (
-                        <Shield size={50} strokeWidth={2} />
-                    ): (
-                        <ShieldOff size={40} strokeWidth={1.9} />
-                    )}
+        <View>
+            <Pressable onPress={setActiveAction} >
+                {({pressed }) => (
+                    <View className="relative flex items-center justify-center w-60 h-60">
+                        <Animated.View className="w-60 h-60 absolute inset-0" style={{
+                            transform: [
+                                { rotate: rotation },
+                                { scale: active ? 1 : 0.90 },
+                            ],
+                        }}>
+                            <Svg viewBox="0 0 250 250" width="100%" height="100%" className="absolute inset-0">
+                            <Path
+                            d="M151.572 33.2059C192.835 15.2847 234.715 57.165 216.794 98.4282L213.831 105.251C208.36 117.848 208.36 132.152 213.831 144.749L216.794 151.572C234.715 192.835 192.835 234.715 151.572 216.794L144.749 213.831C132.152 208.36 117.848 208.36 105.251 213.831L98.4281 216.794C57.165 234.715 15.2847 192.835 33.2059 151.572L36.169 144.749C41.6404 132.152 41.6404 117.848 36.1689 105.251L33.2059 98.4281C15.2847 57.165 57.165 15.2847 98.4282 33.2059L105.251 36.169C117.848 41.6404 132.152 41.6404 144.749 36.1689L151.572 33.2059Z"
+                            fill={active ? "#fba32b" : "#775a32"}
+                            />
+                            </Svg>
+                        </Animated.View>
+                        {active ? (
+                            <Shield size={50} strokeWidth={2} />
+                        ): (
+                            <ShieldOff size={40} strokeWidth={1.9} />
+                        )}
 
-                </View>
-            )}
-
-        </Pressable>
+                    </View>
+                )}
+            </Pressable>
+            <Text className="text-center text-lg">{active ? "Roaming Guard is active." : "Roaming Guard is inactive"}</Text>
+        </View>
     )
 }
